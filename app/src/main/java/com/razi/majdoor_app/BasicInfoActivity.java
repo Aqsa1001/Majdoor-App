547d530e1064829b2d79e94a26f479adc25fad85
Armughan <marmughan01@gmail.com>
2023-03-23 01:29:50 +0500

    Registration Frontend

---
+++ app/src/main/java/com/razi/majdoor_app/BasicInfoActivity.java
@@ -0,0 +1,14 @@
+package com.razi.majdoor_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BasicInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_info);
+    }
+}
---
+++ app/src/main/java/com/razi/majdoor_app/CompanyInfoActivity.java
@@ -0,0 +1,14 @@
+package com.razi.majdoor_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompanyInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company_info);
+    }
+}
